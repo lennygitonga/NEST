40ac018c360e02135e00f276f941a5316a16d4ec
Lenny Gitonga <lennygitonga07@gmail.com>
2026-05-28 12:29:05 +0300

    Add serializers for Agency and AgencyLandlord, including dashboard metrics

---
+++ agencies/serializers.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+from .models import Agency, AgencyLandlord
+from authentication.serializers import UserSerializer
+
+
+class AgencySerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Agency
+        fields = [
+            'id', 'user', 'name', 'registration_number', 'logo',
+            'address', 'phone_number', 'email', 'website',
+            'is_verified', 'commission_rate', 'created_at'
+        ]
+        read_only_fields = ['is_verified', 'commission_rate', 'created_at']
+
+
+class AgencyRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agency
+        fields = [
+            'name', 'registration_number', 'logo',
+            'address', 'phone_number', 'email', 'website'
+        ]
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        agency = Agency.objects.create(user=user, **validated_data)
+        return agency
+
+
+class AgencyVerifySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agency
+        fields = ['is_verified']
+
+
+class AgencyLandlordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AgencyLandlord
+        fields = [
+            'id', 'agency', 'landlord', 'status',
+            'management_fee_percent', 'joined_at'
+        ]
+        read_only_fields = ['joined_at']
+
+
+class AgencyDashboardSerializer(serializers.ModelSerializer):
+    total_properties = serializers.SerializerMethodField()
+    vacant_properties = serializers.SerializerMethodField()
+    occupied_properties = serializers.SerializerMethodField()
+    total_landlords = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Agency
+        fields = [
+            'id', 'name', 'is_verified', 'commission_rate',
+            'total_properties', 'vacant_properties',
+            'occupied_properties', 'total_landlords'
+        ]
+
+    def get_total_properties(self, obj):
+        return obj.properties.count()
+
+    def get_vacant_properties(self, obj):
+        return obj.properties.filter(is_vacant=True).count()
+
+    def get_occupied_properties(self, obj):
+        return obj.properties.filter(is_vacant=False).count()
+
+    def get_total_landlords(self, obj):
+        return obj.landlords.filter(status='ACTIVE').count()
