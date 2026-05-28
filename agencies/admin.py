871a0cf598a7ac1ac0f98d31e06518e10ee43e24
Lenny Gitonga <lennygitonga07@gmail.com>
2026-05-28 12:12:50 +0300

    Add Agency and AgencyLandlord admin configurations for management

--- agencies/admin.py
+++ agencies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Agency, AgencyLandlord
 
-# Register your models here.
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number', 'is_verified', 'commission_rate', 'created_at']
+    list_filter = ['is_verified']
+    search_fields = ['name', 'email', 'registration_number']
+
+@admin.register(AgencyLandlord)
+class AgencyLandlordAdmin(admin.ModelAdmin):
+    list_display = ['agency', 'landlord', 'status', 'management_fee_percent', 'joined_at']
+    list_filter = ['status']
