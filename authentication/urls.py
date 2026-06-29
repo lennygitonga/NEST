4947bc4b9811c0429bafe2c5f30f40c84010fc38
Lenny Gitonga <lennygitonga07@gmail.com>
2026-06-29 06:36:24 +0300

    Add google-login path to authentication URLs

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # Registration & Login
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
+    path('google-login/', views.google_login_view, name='google_login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', views.profile_view, name='profile'),
     path('profile/update/', views.profile_update_view, name='profile_update'),
